d2c1377f0cfda791ea5f5c6a5075eb00cc319f9d
David Fuentes <davidf0827@gmail.com>
2020-08-03 04:50:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,15 @@
 #use key to retreive value 
 
 def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
   
-  
-  name_hash.collect do |key, value|
-    
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+ lowest_key
     
 end
